035b16ea051561097d8344fc0a85bbca7e21d0f7
Larilw <llarissawong@gmail.com>
2022-03-08 22:20:17 -0300

    Ajustes na separação dos simbolos -Correção da separação dos símbolos que removia caracteres especiais como "\n" -Criação de método para ordenar os símbolos de acordo com o número de aparições

--- Huffman.cpp
+++ Huffman.cpp
@@ -1,13 +1,18 @@
 #include "Huffman.h"
 
-No::No(string conteudo, No *esq, No *dir){
-    this->conteudo = conteudo;
+No::No(Simbolo conteudo, No *esq, No *dir){
+    this->conteudo.nAparicoes = conteudo.nAparicoes;
+    this->conteudo.simbolo = conteudo.simbolo;
     this->esq = esq;
     this->dir = dir;
 }
 
 No::~No(){
 
+}
+
+bool No::ehFolha(){
+    return this->esq == NULL && this->dir == NULL;
 }
 
 Arvore::Arvore(No *raiz){
@@ -27,44 +32,85 @@ Simbolo::~Simbolo(){
 
 }
 
-string formatarString(string conteudoArq){
-    int i;
-    int j = 0;
-    string novaString;
-    for(i = 0 ; i < conteudoArq.length() ; i++){
-        if((conteudoArq[i] != '\n') && (conteudoArq[i] != ' ') && (conteudoArq[i] != '\t') && (conteudoArq[i] != '\r')){ 
-            novaString.insert(j, conteudoArq, i, 1);
+void removerNulos(Simbolo simbolos[], Simbolo simbNovo[], int tam, int *n){
+    int i = 0, j = 0;
+    while(i <= tam){
+        if(simbolos[i].nAparicoes != 0){
+            simbNovo[j] = simbolos[i];
             j++;
         }
+        i++;
     }
-    return novaString;
+    *n = j;
 }
 
+void ordenarSimbolos(Simbolo simbolos[], int tam){
+    int i, j;
+    Simbolo x;
+    for(i = 1 ; i < tam ; i++){
+        x = simbolos[i];
+        j = i - 1;
+        while(j >= 0 && x.nAparicoes < simbolos[j].nAparicoes){
+            simbolos[j+1] = simbolos[j];
+            j--;
+        }
+        simbolos[j+1] = x;
+    }
+}
 
-void Huffman::contarAparicoes(string conteudoArq, Simbolo simbolos[256]){
+void Huffman::contarAparicoes(string conteudoArq, Simbolo simbolos[259]){
     int i;
     //Contagem por caractere
     if(this->tipoCodificacao == false){
-        string conteudoFormatado = formatarString(conteudoArq);
-        for(i = 0 ; i < conteudoFormatado.length() ; i++){
-            int pos;
-            pos = 256-(256-conteudoFormatado[i]);
-            simbolos[pos].nAparicoes++;
+        for(i = 0 ; i < conteudoArq.length() ; i++){
+            if((conteudoArq[i] == '\n') || (conteudoArq[i] == '\t') || (conteudoArq[i] == '\r') || (conteudoArq[i] == '\f')){
+                if(conteudoArq[i] == '\n'){
+                    simbolos[256].nAparicoes++;
+                    simbolos[256].simbolo.assign("nl");
+                }
+                else if(conteudoArq[i] == '\t'){
+                    simbolos[257].nAparicoes++;
+                    simbolos[257].simbolo.assign("ht");
+                }
+                else if(conteudoArq[i] == '\r'){
+                    simbolos[258].nAparicoes++;
+                    simbolos[258].simbolo.assign("cr");
+                }
+                    /*
+                    else if(conteudoArq[i] == '\f'){
+                        simbolos[259].nAparicoes++;
+                        simbolos[259].simbolo.assign("np");
+                    }*/
+            }
+            else{
+                int pos;
+                pos = 256-(256-conteudoArq[i]);
+                simbolos[pos].simbolo = char(pos);
+                simbolos[pos].nAparicoes++;
+            }
         }
 
     }
+
 }
 
+void montaArvore(Simbolo simbolos[], int tam){
+
+}
+
+
 int main(){
-    string teste = "Ola meu nome e larissa \n Hoje e domingo \t oioioio \r feee";
+    string teste = "Ola meu nome e larissa \r \rHoje \t e domingo oioioio  eee";
     Huffman novo;
-    Simbolo simbolos[256];
+    Simbolo simbolos[259];
+    Simbolo simbNovo[259];
+    int nSimbolos;
     novo.contarAparicoes(teste, simbolos);
+    removerNulos(simbolos, simbNovo, 259, &nSimbolos);
+    ordenarSimbolos(simbNovo, 18);
     int i = 0;
-    while(i <= 256){
-        if(simbolos[i].nAparicoes != 0){
-            cout<<(char)i<<" "<<simbolos[i].nAparicoes<<endl;
-        }
+    while(i < 18){
+        cout<<simbNovo[i].simbolo<<" "<<simbNovo[i].nAparicoes<<endl;
         i++;
     }
 }
--- Huffman.h
+++ Huffman.h
@@ -2,14 +2,24 @@
 #include <iostream>
 using namespace std;
 
+class Simbolo{
+    public:
+        string simbolo;
+        int nAparicoes;
+
+        Simbolo();
+        ~Simbolo();
+};
+
 class No{
     public:
-        string conteudo;
+        Simbolo conteudo;
         No *esq;
         No *dir;
 
-        No(string conteudo, No *esq, No *dir);
+        No(Simbolo conteudo, No *esq, No *dir);
         ~No();
+        bool ehFolha();
 };
 
 class Arvore{
@@ -21,14 +31,6 @@ class Arvore{
         ~Arvore();
 };
 
-class Simbolo{
-    public:
-        string simbolo;
-        int nAparicoes;
-
-        Simbolo();
-        ~Simbolo();
-};
 
 class Huffman{
     public:
